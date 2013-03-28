5a9dc1551c8c1f4257a0d15da3fa560ad0af2908
Florent Jaby <florent.jaby@gmail.com>
2013-03-28 11:48:42 +0100

    make stream stream a pipable duplex stream

---
+++ test/test-pipe.js
@@ -0,0 +1,41 @@
+var util = require('util');
+var stream = require('stream');
+var SS = require('../');
+var sink = require('./sink');
+
+function EmitStream (array) {
+    stream.Readable.call(this, {objectMode:true});
+    this._source = array.slice();
+}
+util.inherits(EmitStream, stream.Readable);
+EmitStream.prototype._read = function(size) {
+    var chunk = this._source.shift();
+    if(!chunk) this.push(null);
+
+    var s = new stream.PassThrough();
+    process.nextTick(function() {
+        s.end(chunk);
+    });
+    this.push(s);
+}
+
+exports.testPipingStreams = function(test) {
+    var emitter = new EmitStream(['hello', ' world', '!']);
+    var ss = SS();
+    var s = sink();
+    var done = false;
+    var to = setTimeout(function() {
+        if(!done) {
+            test.fail('No end detected')
+            test.done();
+        }
+    }, 500);
+
+    emitter.pipe(ss).pipe(s).on('data', function(data) {
+        test.equal(data, "hello world!", "Data in sink should be identical");
+        done = true;
+        test.done();
+        clearTimeout(to);
+    });
+
+}
