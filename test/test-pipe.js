edfd1fae8c5e73878ee6abdfc8a853b52fdcf828
Florent Jaby <fjaby@octo.com>
2013-07-14 19:31:44 +0200

    fix faulty test

--- test/test-pipe.js
+++ test/test-pipe.js
@@ -2,6 +2,23 @@ var util = require('util');
 var stream = require('stream');
 var SS = require('../');
 var sink = require('stream-sink');
+var domain = require('domain');
+
+Function.prototype.withDomain = function(withStack) {
+  var fn = this;
+  return function(test) {
+    var d = domain.create();
+    d.on('error', function(e) {
+      test.fail('test failed with ' + e.message);
+      if(withStack) {
+        console.error(e.stack)
+      }
+      test.done();
+    });
+    d.run(fn.bind(this, test));
+  }
+}
+
 
 function EmitStream (array) {
     stream.Readable.call(this, {objectMode:true});
@@ -10,13 +27,13 @@ function EmitStream (array) {
 util.inherits(EmitStream, stream.Readable);
 EmitStream.prototype._read = function(size) {
     var chunk = this._source.shift();
-    if(!chunk) this.push(null);
+    if(!chunk) return this.push(null);
 
     var s = new stream.PassThrough();
     process.nextTick(function() {
         s.end(chunk);
     });
-    this.push(s);
+    this.push(s)
 }
 
 exports.testPipingStreams = function(test) {
@@ -38,4 +55,4 @@ exports.testPipingStreams = function(test) {
         clearTimeout(to);
     });
 
-}
+}.withDomain(true)
